71c672264c9fa25bff0034d5ed45ea268706a1db
Ryan Conrad <camalot@gmail.com>
2021-06-10 12:03:53 -0500

    added a local run script for testing.

---
+++ local-run.sh
@@ -0,0 +1,16 @@
+
+
+ docker run \
+  --name aaaaaaaaaaaaaa \
+  --label aaaaa \
+  --workdir /github/workspace \
+  --rm \
+  -e INPUT_FOLDER=MedalOverlay \
+  -e INPUT_VERSION=1.0.0-snapshot \
+  -e HOME=/github/home \
+  -e GITHUB_REPOSITORY=chatbot-medaloverlay \
+  -e GITHUB_REPOSITORY_OWNER=camalot \
+  -e CI=true \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  -v /home/rconr/Development/chatbot-medaloverlay:/github/workspace \
+  cbgpa:local
